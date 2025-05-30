2e6c822be5d92dedabe3d7e2fef8540dd23eed7b
andrii_solomka <andriissolomka@gmail.com>
2025-05-30 22:09:29 +0300

    Add cookie management with CookieService and CookieModule

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ensureLogDirExists } from './utils/logger/logger.config';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 ensureLogDirExists();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
 
